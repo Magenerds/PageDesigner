5628cb40b6509714c205bc2f8e47408897ea2e32
Simon Sippert <s.sippert@techdivision.com>
2019-06-24 15:29:26 +0200

    Add missing transformation of wysiwyg widget in json field

--- Setup/UpgradeData.php
+++ Setup/UpgradeData.php
@@ -9,6 +9,7 @@
 
 namespace Magenerds\PageDesigner\Setup;
 
+use Magenerds\PageDesigner\Constants;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\UpgradeDataInterface;
@@ -43,8 +44,8 @@ class UpgradeData implements UpgradeDataInterface
 
             // define tables to update
             $tables = [
-                'cms_page' => 'content',
-                'cms_block' => 'content',
+                'cms_page' => Constants::ATTR_CONTENT,
+                'cms_block' => Constants::ATTR_CONTENT,
                 'widget_instance' => 'instance_type',
             ];
 
@@ -54,6 +55,25 @@ class UpgradeData implements UpgradeDataInterface
                 $setup->getConnection()->query(sprintf(
                     "UPDATE IGNORE %s SET %s = REPLACE(%s, '%s', '%s') WHERE %s LIKE '%%%s%%' ESCAPE '|'",
                     $setup->getTable($table), $field, $field, $replacedWidget, $newWidget, $field, $replacedWidget
+                ));
+            }
+        }
+
+        // end setup
+        $setup->endSetup();
+
+        // version upgrade to 2.1.2
+        if (version_compare($context->getVersion(), '2.1.2') < 0) {
+            // replace old wysiwyg widget
+            $replacedWidget = 'Magenerds\\\\\\\\PageDesigner\\\\\\\\Block\\\\\\\\Widget\\\\\\\\Editor';
+            $newWidget = 'Magenerds\\\\\\\\WysiwygWidget\\\\\\\\Block\\\\\\\\Widget\\\\\\\\Editor';
+
+            // iterate over tables
+            foreach (Constants::CONTENT_TABLES as $table) {
+                /** @noinspection SqlNoDataSourceInspection */
+                $setup->getConnection()->query(sprintf(
+                    "UPDATE IGNORE %s SET %s = REPLACE(%s, '%s', '%s') WHERE %s LIKE '%%%s%%' ESCAPE '|'",
+                    $setup->getTable($table), Constants::ATTR_PAGE_DESIGNER_JSON, Constants::ATTR_PAGE_DESIGNER_JSON, $replacedWidget, $newWidget, Constants::ATTR_PAGE_DESIGNER_JSON, $replacedWidget
                 ));
             }
         }
